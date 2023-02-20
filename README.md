f35e507798a77ffc1f4e6198451441b82cce0578
VentGrey <VentGrey@users.noreply.github.com>
2023-02-20 00:22:46 +0000

    deploy: 5d416917b37c13eeef35b3301e581d97c5bfea21

--- README.md
+++
@@ -1,3 +0,0 @@
-# Epitaph GitHub Page
-
-This repository is for maintaining the Epitaph's project main website. No issues / PR's will be accepted as this site is not meant to change that much.
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import App from './App.svelte';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./app.css";
-
-const app = new App({
-  target: document.getElementById('app'),
-})
-
-export default app
