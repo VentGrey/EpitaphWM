26ffb92c9d92db24db28e6c1bca3bdee91736950
VentGrey <omarpurataf@gmail.com>
2023-02-19 18:40:08 -0600

    Try with full path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/EpitaphWM/",
+  base: "https://ventgrey.github.io/EpitaphWM/",
   plugins: [svelte()],
 })
